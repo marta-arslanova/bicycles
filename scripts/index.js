97f824cd592a1bd88ee2bc7ee48c5cf3fbac62d3
marta <marti.arslanova@gmail.com>
2021-12-12 22:35:38 +0300

    Адаптив под мобильные устройства

--- scripts/index.js
+++ scripts/index.js
@@ -7,6 +7,7 @@ const swiper = new Swiper('.swiper', {
     prevEl: '.swiper-button-prev',
   },
 });
+
 // Данные карточкек
 
 const cards = {
@@ -15,33 +16,40 @@ const cards = {
       name: 'Модель велосипеда',
       link: 'images/SpecializedS-WorksShiv.png',
       descriptionModel: 'Specialized S-Works Shiv',
+      webkitURL: 'https://www.sigmasports.com/item/Specialized/S-Works-Shiv-Disc-Limited-Edition-Triathlon-Bike-2019/K8P9'
     },
     {
       name: 'Модель велосипеда',
       link: 'images/BMCTimemachine01ONE.png',
       descriptionModel: 'BMC Timemachine 01 ONE',
+      webkitURL: 'https://www.sigmasports.com/item/BMC/Timemachine-01-One-Force-Disc-TT-Triathlon-Bike-2021/S835',
     },
     {
       name: 'Модель велосипеда',
       link: 'images/CerveloP-Series.png',
       descriptionModel: 'Cervelo P-Series',
+      webkitURL: 'https://www.sigmasports.com/item/Cervelo/P-Series-Ultegra-Di2-TT-Triathlon-Bike-2021/RM6Q',
     },
+
   ],
-  gravel: [
+    gravel: [
     {
       name: 'Модель велосипеда',
       link: 'images/CerveloAsperoGRX810.png',
       descriptionModel: 'Cervelo Aspero GRX 810',
+      webkitURL: 'https://www.sigmasports.com/item/Cervelo/Aspero-GRX-810-1x-Disc-Gravel-Bike-2021/RJDE',
     },
     {
       name: 'Модель велосипеда',
       link: 'images/SpecializedS-WorksDiverge.png',
       descriptionModel: 'Specialized S-Works Diverge',
+      webkitURL: 'https://www.sigmasports.com/item/Specialized/S-Works-Diverge-Gravel-Bike-2020/NVJ9',
     },
     {
       name: 'Модель велосипеда',
       link: 'images/CannondaleTopstoneLefty3.png',
       descriptionModel: 'Cannondale Topstone Lefty 3',
+      webkitURL: 'https://www.sigmasports.com/item/Cannondale/Topstone-Carbon-Lefty-3-Disc-Gravel-Road-Bike-2021/PUC8',
     },
   ],
   highway: [
@@ -49,17 +57,21 @@ const cards = {
       name: 'Модель велосипеда',
       link: 'images/CerveloCaledonia-5.png',
       descriptionModel: 'Cervelo Caledonia-5',
+      webkitURL: 'https://www.sigmasports.com/item/Cervelo/Caledonia-5-Ultegra-Disc-Road-Bike-2021/RDEN ',
     },
     {
       name: 'Модель велосипеда',
       link: 'images/CannondaleSystemsixHimod.png',
       descriptionModel: 'Cannondale Systemsix Himod',
+      webkitURL: 'https://www.sigmasports.com/item/Cannondale/SystemSix-HiMOD-Ultegra-Di2-Disc-Road-Bike-2021/R82J',
     },
     {
       name: 'Модель велосипеда',
       link: 'images/TrekDomaneSL-7.png',
       descriptionModel: 'Trek Domane SL-7',
+      webkitURL: 'https://www.sigmasports.com/item/Trek/Domane-SL-7-Force-eTap-AXS-Disc-Road-Bike-2021/RULF',
     },
+
   ],
 };
 
@@ -91,9 +103,11 @@ function createBicycleCard(card) {
   const cardBicycleElement = bicycleCardTemplate.querySelector('.bicycle-card').cloneNode(true);
   const cardBicycleImage = cardBicycleElement.querySelector('.bicycle-card__image');
   const cardBicycleDescr = cardBicycleElement.querySelector('.bicycle-card__descr');
+  const cardBicycleLink = cardBicycleElement.querySelector('.bicycle-card__link');
   cardBicycleImage.src = card.link;
   cardBicycleImage.alt = card.name;
   cardBicycleDescr.textContent = card.descriptionModel;
+  cardBicycleLink.textContent = card.url;
   return cardBicycleElement;
 }
 
